b10d512644b70fbd3fffa01db0a44a89e169adb4
JordanYefet <47048437+JordanYefet@users.noreply.github.com>
2021-12-19 11:30:07 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIAQVDKZZLZKVDECEUM"
-  secret_key = "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"
-  region     = "eu-central-1"
+  access_key = "<access_key>"
+  secret_key = "<secret_key>"
+  region     = "<region>"
 }
